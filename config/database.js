84fa1003942b170392f9dda4090a4afa4ceeedad
Sancho Sebastine <sebastine.sancho@gmail.com>
2015-03-28 23:43:40 -0500

    Sancho - heroku changes part 5, createDbSettings

--- config/database.js
+++ config/database.js
@@ -1,14 +1,15 @@
 
 function createDBSettings(mongoLabURI) {
     var dbSettings = {};
+    console.log('mongoLabUri isssssss: ' + mongoLabURI);
     var regexp = /^mongodb:\/\/(\w+):(\w+)@(\w+):(\w+)\/(\w+)$/;
     var matches = mongoLabURI.match(regexp);
 
-    dbSettings.db = matches[5];
     dbSettings.host = matches[3];
     dbSettings.port = matches[4];
     dbSettings.username = matches[1];
     dbSettings.password = matches[2];
+    dbSettings.db = matches[5];
 
     return dbSettings;
 }
@@ -16,7 +17,7 @@ function createDBSettings(mongoLabURI) {
 var dbConfig = {db: 'userSessions'}
 
 if(process.env.MONGOLAB_URI) {
-  dbConfig = createDBSettings(process.env.MONGOLAB_URI);
+  dbConfig = createDBSettings(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL);
 }
 
 module.exports = {
--- spec/CardSpec.js
+++ spec/CardSpec.js
@@ -1,9 +1,34 @@
 var Card = require('../model/card');
 
+function createDBSettings(mongoLabURI) {
+    var dbSettings = {};
+    var regexp = /^mongodb:\/\/(\w+):(\w+)@(\w+):(\w+)\/(\w+)$/;
+    var matches = mongoLabURI.match(regexp);
+
+    dbSettings.db = matches[5];
+    dbSettings.host = matches[3];
+    dbSettings.port = matches[4];
+    dbSettings.username = matches[1];
+    dbSettings.password = matches[2];
+
+    return dbSettings;
+}
+
 describe("Card", function() {
   beforeEach(function() {
     
   });
+
+
+  it("should be able to parse mongoLab uri", function() {
+        var dbSettings = createDBSettings('mongodb://dbuser:dbpass@host:port/dbname');
+        expect(dbSettings.db).toEqual('dbname');
+        expect(dbSettings.username).toEqual('dbuser');
+        expect(dbSettings.password).toEqual('dbpass');
+        expect(dbSettings.port).toEqual('port');
+        expect(dbSettings.host).toEqual('host');
+  });
+
 
   it("should be able to get Rank and Suit in a card", function() {
     var card = new Card('8', 'S');
