cb05e972f0d08da3497384768791304e0cf3ec94
Sancho Sebastine <sancho.nitw@gmail.com>
2015-05-23 17:05:12 -0500

    fixing the callback url to kalikku.com

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '224849100897425', // your App ID
         'clientSecret'  : '129967f82a67563dd0267e82b9ee443e', // your App Secret
-        'callbackURL'   : 'http://localhost:5000/auth/facebook/callback'
+        'callbackURL'   : 'http://kalikku.herokuapp.com/auth/facebook/callback'
     }
 };
